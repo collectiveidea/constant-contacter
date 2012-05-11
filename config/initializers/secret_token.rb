fab0f3587fc45697ef57695f95e8bd410a692850
Daniel Morrison <daniel@collectiveidea.com>
2012-05-11 15:51:24 -0400

    Initial project with some gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConstantContacter::Application.config.secret_token = 'fbd975cbc7978cd77bfe591514c8a0eaaf224b3a8d6313370ab75d540cec55cc8d41c1e9bf1cc21301b2edb6bba545a71a48afeb9f9b81047f96e2f48ebe13f6'
