36d6675ea74d51445231a9e1f0fa926db6184d77
Daniel Morrison <daniel@collectiveidea.com>
2012-05-11 16:50:10 -0400

    Setup redirect

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,10 @@
 class EmailsController < ApplicationController
   def create
     List.find_by_name!(params[:name]).add_email(params)
+    if params[:return].present?
+      redirect_to params[:return]
+    else
+      render :text => 'Thank you!'
+    end
   end
 end
